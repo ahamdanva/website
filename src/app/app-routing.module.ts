ab7ac837cf272c72de2d9aa155f630741b7ce626
danav <ahamdanva682@gmail.com>
2021-06-11 17:41:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { DecryptComponent } from './decrypt/decrypt.component';
+import { EncryptComponent } from './encrypt/encrypt.component';
+import { IdentifyComponent } from './identify/identify.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'encrypt',component:EncryptComponent},
+  {path:'',component:ContainerComponent},
+  {path:'identify',component:IdentifyComponent},
+  {path:'decrypt',component:DecryptComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncryptComponent } from './encrypt/encrypt.component';
+import { ContainerComponent } from './container/container.component';
+import { IdentifyComponent } from './identify/identify.component';
+import { DecryptComponent } from './decrypt/decrypt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncryptComponent,
+    ContainerComponent,
+    IdentifyComponent,
+    DecryptComponent
   ],
   imports: [
     BrowserModule,
